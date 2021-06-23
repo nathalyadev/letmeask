be88d19813d5172c991a06966bee4de627c57e57
Nathgit198 <nath.conta14@gmail.com>
2021-06-23 15:44:20 -0300

    Aula 2 (Completa)

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,38 +1,41 @@
-import illustrationImg from '../assets/images/illustration.svg';
+import { Link } from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
+// import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
-export function NewRoom () {
-    return (
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="Letmeask" />
-                        <img src={googleIconImg} alt="Logo do google" />
-                        <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input 
-                        type="text" 
-                        placeholder="Nome da sala"
-                        />
-                        <Button type="submit">
-                          Criar sala
-                        </Button>
-                    </form>
-                    <p>
-                        Quer entrar em uma sala existente? <a href="#">Clique Aqui</a>
-                    </p>
-                </div>
-            </main>
+export function NewRoom() {
+  // const { user } = useAuth()
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input 
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
         </div>
-    )
+      </main>
+    </div>
+  )
 }
